0676a2f16dcce6dd8f3bc49afe14cc4da17eff05
Samuel <samuel.ravard@etu.univ-nantes.fr>
2023-11-26 14:02:07 +0100

    "fix"

--- Old/views/layout.views.php
+++ Old/views/layout.views.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-      ....
 </head>
 <body>
     <div class="container">
         <div class="row">
-            <h1 class="mt-5 text-left"><?=$view_params['titre']?></h1>
-            <?php include("$name.view.php");?>
+            <h1 class="mt-5 text-left"><?= $titre ?></h1>
+            <?php include("$name.view.php"); ?>
         </div>
     </div>
 </body>
