e1ca4171fb7c58c365b1911c777a29f88de567b8
Samuel <samuel.ravard@etu.univ-nantes.fr>
2023-12-12 17:50:48 +0100

    login update

--- php/views3/login.view.php
+++ php/views3/login.view.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Formulaire d'authentification</title>
 </head>
 <body>
 
-    <h2>Login</h2>
+    <h2>Formulaire d'authentification</h2>
 
     <form action="login.php" method="post">
         <label for="username">Email :</label>
