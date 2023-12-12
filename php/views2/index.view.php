ca0cb58441a9011cf369e6d42352a4ba5b2492a6
Samuel <samuel.ravard@etu.univ-nantes.fr>
2023-12-12 16:46:11 +0100

    fix

--- php/views2/index.view.php
+++ php/views2/index.view.php
@@ -2,5 +2,5 @@
 $param_vue = array('titre' => 'Titre de la page');
 
 // Utilisation de la fonction view en passant les données en tant que paramètres
-view('views/layout.views.php', $param_vue);
+view('views2/layout.views.php', $param_vue);
 ?>
