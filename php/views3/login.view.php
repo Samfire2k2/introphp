16b9d1adc9d89b5b96e8770f2672026d130ff492
Samuel <samuel.ravard@etu.univ-nantes.fr>
2023-12-12 17:49:30 +0100

    update formulaire

--- php/views3/login.view.php
+++ php/views3/login.view.php
@@ -10,10 +10,10 @@
     <h2>Login</h2>
 
     <form action="login.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
+        <label for="username">Email :</label>
+        <input type="email" id="username" name="username" required>
         <br>
-        <label for="password">Password:</label>
+        <label for="password">Mot de passe :</label>
         <input type="password" id="password" name="password" required>
         <br>
         <button type="submit">Login</button>
