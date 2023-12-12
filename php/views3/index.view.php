49671e9b17250c965a2192c1381f505edc5af52e
Samuel <samuel.ravard@etu.univ-nantes.fr>
2023-12-12 16:49:13 +0100

    tp3 maj

---
+++ php/views3/index.view.php
@@ -0,0 +1,6 @@
+<?php
+$param_vue = array('titre' => 'Titre de la page');
+
+// Utilisation de la fonction view en passant les données en tant que paramètres
+view('views3/layout.views.php', $param_vue);
+?>
