6693bfaa33c1612c33dfd8cc1f1109d67ad6e29f
Samuel <samuel.ravard@etu.univ-nantes.fr>
2023-12-12 17:56:55 +0100

    login

--- php/views3/login.view.php
+++ php/views3/login.view.php
@@ -4,16 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire d'authentification</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
 <body>
 
     <h2>Formulaire d'authentification</h2>
 
-    <form action="login.php" method="post">
-        <label for="username">Email :</label>
+    <form class="form" action="login.php" method="post">
+        <label for="username">Email :</label><br>
         <input type="email" id="username" name="username" required>
         <br>
-        <label for="password">Mot de passe :</label>
+        <label for="password">Mot de passe :</label><br>
         <input type="password" id="password" name="password" required>
         <br>
         <button type="submit">Login</button>
