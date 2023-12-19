3c0f14625b309c549cdd0552ab4a0eecc4d5f627
Samfire2k2 <samuelravard@gmail.com>
2023-12-19 15:38:46 +0100

    Update file paths and fix code formatting

--- php/views4/show.view.php
+++ php/views4/show.view.php
@@ -6,7 +6,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-<?php if ($_SERVER['REQUEST_METHOD'] == 'POST'): ?>
     <h1>Informations du module</h1>
 
 <table class="table">
@@ -22,17 +21,6 @@
     </tr>
 </thead>
 <tbody>
-<<<<<<< HEAD
-
-    <tr>
-        <td><?= isset($code) ? $code : '' ?></td>
-        <td><?= isset($nom) ? $nom : ''?></td>
-        <td><?= isset($responsable) ? $responsable : '' ?></td>
-        <td><?= isset($email_responsable) ? $email_responsable : '' ?></td>
-        <td><?= isset($cm) ? $cm : '' ?></td>
-        <td><?= isset($td) ? $td : '' ?></td>
-        <td><?= isset($tp) ? $tp : '' ?></td>
-=======
     <?php foreach ($modules as $module): ?>
     <tr>
         <td><?= $module['code'] ?></td>
@@ -42,15 +30,9 @@
         <td><?= $module['CM'] ?></td>
         <td><?= $module['TD'] ?></td>
         <td><?= $module['TP'] ?></td>
->>>>>>> 642a5f5ff06db37b2559337bdbb481b080826cea
     </tr>
     <?php endforeach; ?>
 </tbody>
 </table>
 </body>
-<<<<<<< HEAD
-<?php endif; ?>
 </html>
-=======
-</html>
->>>>>>> 642a5f5ff06db37b2559337bdbb481b080826cea
